e85cd95fc52cedb92e424ca9c0bc9f0a8da8d3e2
anhaoxiong <anhaoxiong@innotechx.com>
2020-05-08 19:08:58 +0800

    add iOS project

---
+++ Xcode/Demo/Demo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Demo
+//
+//  Created by ahx on 2020/5/8.
+//  Copyright © 2020 ahx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Xcode/Demo/Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by ahx on 2020/5/8.
+//  Copyright © 2020 ahx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
